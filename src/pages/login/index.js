225db94710d572d9fc2d4c2db1b9ea40f681f2d0
Jaqueline Paschoal <jaqueline.paschoal@scaffoldeducation.com.br>
2019-02-05 08:31:13 -0200

    form validation using formik

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,36 +1,56 @@
-import React, { Component } from "react";
-// import PropTypes from "prop-types";
+import React from "react";
+import { withFormik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-import { Image, ButtonContent, Form } from "./style";
 import { Container } from "../../styles/global";
-
-import { Link } from "react-router-dom";
-
+import { Image } from "./style";
 import Coffee from "../../assets/coffee.svg";
 
-class Login extends Component {
-  render() {
-    return (
-      <Container>
-        <Image>
-          <img src={Coffee} alt="Coffee" />
-        </Image>
-        <Form>
-          <div>
-            <label>Nome completo</label>
-            <input type="text" />
-          </div>
-          <div>
-            <label>E-mail</label>
-            <input type="text" />
-          </div>
-        </Form>
-        <ButtonContent>
-          <Link to={"/recipes"}>Cadastrar</Link>
-        </ButtonContent>
-      </Container>
-    );
-  }
-}
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .required("Informe o nome!")
+    .min(5, "O nome deve conter mais de 5 letras!")
+    .max(100, "O nome deve conter menos de 100 letras!")
+    .notOneOf(["admin", "administrador"], "Esse nome não pode camarada!"),
+  email: Yup.string()
+    .required("Informe o email!")
+    .email("Informe um email válido!")
+});
 
-export default Login;
+const enhanceWithFormik = withFormik({
+  mapPropsToValues: () => ({ name: "", email: "" }),
+  handleSubmit: values => {
+    console.log(values);
+  },
+  isInitialValid: false,
+  validateOnChange: true,
+  validateOnBlur: true,
+  displayName: "MyForm",
+  validationSchema: schema
+});
+
+const Login = props => {
+  return (
+    <Container>
+      <Image>
+        <img src={Coffee} alt="Coffee" />
+      </Image>
+      <Form>
+        <div>
+          <label>Nome completo</label>
+          <Field name="name" placeholder="Nome" /> <br />
+          <ErrorMessage name="name" />
+        </div>
+        <div>
+          <label>Nome completo</label>
+          <Field name="email" placeholder="Email" />
+          <br />
+          <ErrorMessage name="email" />
+        </div>
+        <button type="submit">Enviar</button>
+      </Form>
+    </Container>
+  );
+};
+
+export default enhanceWithFormik(Login);
